da67b344be72e9e5a5906b77c532f3f177bba2c9
neetdai <ake17101@gmail.com>
2019-04-13 16:25:17 +0800

    修改 problem58

--- src/problems/problem58.rs
+++ src/problems/problem58.rs
@@ -1,38 +1,46 @@
-// 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+/// 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
 
-// 如果不存在最后一个单词，请返回 0 。
+/// 如果不存在最后一个单词，请返回 0 。
 
-// 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+/// 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
 
-// 示例:
+/// ## Examples:
+/// ### Example1:
+/// 输入: "Hello World"
+/// 输出: 5
+/// ```rust
+/// use leetcode_SAO::problems::problem58::{length_of_last_word};
+/// assert_eq!(
+///     length_of_last_word(String::from("Hello World")),
+///     5
+/// );
+/// ```
+/// 
+/// ### Example2:
+/// 输入: "Hello "
+/// 输出: 5
+/// ```rust
+/// use leetcode_SAO::problems::problem58::{length_of_last_word};
+/// assert_eq!(length_of_last_word(String::from("Hello ")), 5);
+/// ```
+/// 
+/// ### Example3:
+/// 输入: ""
+/// 输出: 0
+/// ```rust
+/// use leetcode_SAO::problems::problem58::{length_of_last_word};
+/// assert_eq!(length_of_last_word(String::from("")), 0);
+/// ```
 
-// 输入: "Hello World"
-// 输出: 5
-// 
 
-struct Solution{}
+pub fn length_of_last_word(s: String) -> i32 {
+    let list: Vec<&str> = s.split_whitespace().collect();
+    let length: usize = list.len();
 
-impl Solution {
-    pub fn length_of_last_word(s: String) -> i32 {
-        let list: Vec<&str> = s.split_whitespace().collect();
-        let length: usize = list.len();
-
-        if length == 0 {
-            0
-        } else {
-            list[length - 1].len() as i32
-        }
+    if length == 0 {
+        0
+    } else {
+        list[length - 1].len() as i32
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_length_of_last_word() {
-        assert_eq!(Solution::length_of_last_word(String::from("Hello World")), 5);
-        assert_eq!(Solution::length_of_last_word(String::from("Hello ")), 5);
-        assert_eq!(Solution::length_of_last_word(String::from("")), 0);
-    }
-}
